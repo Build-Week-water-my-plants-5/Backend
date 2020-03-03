338c042f2cf4b835442cb3eba88a4f9cfb57e7ce
Kronykus <MoreheadSteve@gmail.com>
2020-03-03 17:25:17 -0600

    formatting and adding back removed section

--- api/auth/routers/router.js
+++ api/auth/routers/router.js
@@ -1,5 +1,7 @@
-const router     = require( 'express' ).Router();
-const authRouter = require( './auth-router' );
+const router         = require( 'express' ).Router();
+const authRouter     = require( './auth-router'      );
+const userRouter     = require( './user-router'      );
+const authMiddleWare = require( '../auth-middleware' );
 //===============================================================>
 // Use
 //=====================>
